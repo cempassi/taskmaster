486ea78766568253447160d10ed2f557ad64c044
Cedric M'Passi <cedricmpassi@gmail.com>
2020-11-02 15:19:32 +0100

    Read from .toml file into ReadConfig

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Deserialize)]
+struct ReadConfig {
+    numprocess: i32,
+    umask: i32,
+    stopsignal: String,
+}
+
 fn main() {
     println!("Hello, world!");
+    let content = fs::read_to_string("./task.toml").unwrap();
+    let config: ReadConfig = toml::from_str(&content).unwrap();
+
+    println!(
+        "config.numprocess: {}, config.umask: {}, config.stopsignal: {:?}",
+        config.numprocess, config.umask, config.stopsignal
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
