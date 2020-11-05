ae1f60fb73fed4f5394e928a2988f0119bfc1539
Cedric M'Passi <cedricmpassi@gmail.com>
2020-11-05 15:54:43 +0100

    Signals addition and file splitting

--- src/config.rs
+++ src/config.rs
@@ -1,64 +1,136 @@
-mod// enum des signaux
+use serde::Deserialize;
+use std::convert::From;
+
+#[derive(Debug)]
 pub enum Signal {
-	SIGHUP,
-	SIGINT,
-	SIGQUIT,
-	SIGILL,
-	SIGTRAP,
-	SIGABRT,
-	SIGBUS,
-	SIGFPE,
-	SIGKILL,
-	SIGUSR1,
-	SIGSEGV,
-	SIGUSR2,
-	SIGPIPE,
-	SIGALRM,
-	SIGTERM,
-	SIGSTKFLT,
-	SIGCHLD,
-	SIGCONT,
-	SIGSTOP,
-	SIGTSTP,
-	SIGTTIN,
-	SIGTTOU,
-	SIGURG
-}
-}
-
-// Status d'un job
-enum State {
-	RUNNING,
-	STOPPED,
-	EXITED,
-	KILLED
-}
-
-// Enum des instructions à envoyer sur le task 
-enum Instruction {
-	START,
-	RESTART,
-	STOP,
-	STATUS,
-	SHUTDOWN
+    SIGHUP,
+    SIGINT,
+    SIGQUIT,
+    SIGILL,
+    SIGTRAP,
+    SIGABRT,
+    SIGEMT,
+    SIGFPE,
+    SIGKILL,
+    SIGBUS,
+    SIGSEGV,
+    SIGSYS,
+    SIGPIPE,
+    SIGALRM,
+    SIGTERM,
+    SIGURG,
+    SIGSTOP,
+    SIGTSTP,
+    SIGCONT,
+    SIGCHLD,
+    SIGTTIN,
+    SIGTTOU,
+    SIGIO,
+    SIGXCPU,
+    SIGXFSZ,
+    SIGVTAL,
+    SIGPROF,
+    SIGWINC,
+    SIGINFO,
+    SIGUSR1,
+    SIGUSR2,
 }
 
-
-// Un enum pour les erreurs pas mal pour la gestion et centraliser les messages
-enum Error {
-	InvalidCmd
+#[derive(Debug, Deserialize)]
+pub struct ReadConfig {
+    numprocess: i32,
+    umask: i32,
+    stopsignal: String,
+    workingdir: String,
 }
 
-// Structure du gestionnaire de job control avec le fichier de conf
-struct taskmaster {
-	configFile: str,
-
+#[derive(Debug)]
+pub struct Config {
+    numprocess: i32,
+    umask: i32,
+    stopsignal: Signal,
+    workingdir: String,
 }
 
-// Structure de job avec la commande, le status, l'option d autorestart et le starttime (à completer)
-struct Job {
-	cmd: Cmd,
-	state: State,
-	autorestart: bool,
-	starttime: temps(),
+impl From<ReadConfig> for Config {
+    fn from(readconf: ReadConfig) -> Self {
+        let signal: Option<Signal> = match readconf.stopsignal.as_str() {
+            "HUP" => Some(Signal::SIGHUP),
+            "INT" => Some(Signal::SIGINT),
+            "QUIT" => Some(Signal::SIGQUIT),
+            "ILL" => Some(Signal::SIGILL),
+            "TRAP" => Some(Signal::SIGTRAP),
+            "ABRT" => Some(Signal::SIGABRT),
+            "EMT" => Some(Signal::SIGEMT),
+            "FPE" => Some(Signal::SIGFPE),
+            "KILL" => Some(Signal::SIGKILL),
+            "BUS" => Some(Signal::SIGBUS),
+            "SEGV" => Some(Signal::SIGSEGV),
+            "SYS" => Some(Signal::SIGSYS),
+            "PIPE" => Some(Signal::SIGPIPE),
+            "ALRM" => Some(Signal::SIGALRM),
+            "TERM" => Some(Signal::SIGTERM),
+            "URG" => Some(Signal::SIGURG),
+            "STOP" => Some(Signal::SIGSTOP),
+            "TSTP" => Some(Signal::SIGTSTP),
+            "CONT" => Some(Signal::SIGCONT),
+            "CHLD" => Some(Signal::SIGCHLD),
+            "TTIN" => Some(Signal::SIGTTIN),
+            "TTOU" => Some(Signal::SIGTTOU),
+            "IO" => Some(Signal::SIGIO),
+            "XCPU" => Some(Signal::SIGXCPU),
+            "XFSZ" => Some(Signal::SIGXFSZ),
+            "VTAL" => Some(Signal::SIGVTAL),
+            "PROF" => Some(Signal::SIGPROF),
+            "WINC" => Some(Signal::SIGWINC),
+            "INFO" => Some(Signal::SIGINFO),
+            "USR1" => Some(Signal::SIGUSR1),
+            "USR2" => Some(Signal::SIGUSR2),
+            &_ => None,
+        };
+
+        Config {
+            numprocess: readconf.numprocess,
+            umask: readconf.umask,
+            stopsignal: signal.unwrap(),
+            workingdir: readconf.workingdir,
+        }
+    }
 }
+
+//Status d'un job
+//enum State {
+//	RUNNING,
+//	STOPPED,
+//	EXITED,
+//	KILLED
+//}
+//
+//// Enum des instructions à envoyer sur le task
+//enum Instruction {
+//	START,
+//	RESTART,
+//	STOP,
+//	STATUS,
+//	SHUTDOWN
+//}
+//
+//
+//// Un enum pour les erreurs pas mal pour la gestion et centraliser les messages
+//enum Error {
+//	InvalidCmd
+//}
+//
+//// Structure du gestionnaire de job control avec le fichier de conf
+//struct taskmaster {
+//	configFile: str,
+//
+//}
+//
+//// Structure de job avec la commande, le status, l'option d autorestart et le starttime (à completer)
+//struct Job {
+//	cmd: Cmd,
+//	state: State,
+//	autorestart: bool,
+//	starttime: temps(),
+//}
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,18 @@
-use serde::Deserialize;
 use std::fs;
-use std::convert::From;
 
-#[derive(Debug)]
-pub enum Signal {
-    SIGTERM,
-    SIGSEGV
-}
+mod config;
 
-#[derive(Deserialize)]
-struct ReadConfig {
-    numprocess: i32,
-    umask: i32,
-    stopsignal: String,
-    workingdir: String
-}
-#[derive(Debug)]
-pub struct Config {
-    numprocess: i32,
-    umask: i32,
-    stopsignal: Signal,
-    workingdir: String
-}
-
-impl From<ReadConfig> for Config {
-    fn from(readconf: ReadConfig) -> Self {
-        
-        let signal: Option<Signal> = match readconf.stopsignal.as_str(){
-             "TERM" => Some(Signal::SIGTERM),
-             "SEGV" => Some(Signal::SIGSEGV),
-             &_ => None
-        };
-
-        Config {numprocess: readconf.numprocess,
-                umask: readconf.umask,
-                stopsignal: signal.unwrap(),
-                workingdir: readconf.workingdir
-        }
-    }
-}
+use crate::config::{ReadConfig, Config};
 
 fn main() {
-    println!("Hello, world!");
     let content = fs::read_to_string("./task.toml").unwrap();
     let readconfig: ReadConfig = toml::from_str(&content).unwrap();
 
-    println!(
-        "readconfig.numprocess: {}, readconfig.umask: {}, readconfig.stopsignal: {:?}, readconfig.wokingdir: {:?}",
-        readconfig.numprocess, readconfig.umask, readconfig.stopsignal, readconfig.workingdir
-    );
+    println!("readconfig: {:?}", readconfig);
 
     let config: Config = readconfig.into();
-    
-    println!(
-        "config.numprocess: {}, config.umask: {}, config.stopsignal: {:?}, config.wokingdir: {:?}",
-        config.numprocess, config.umask, config.stopsignal, config.workingdir
-    );
+
+    println!("config: {:?}", config );
 }
 
 #[cfg(test)]
