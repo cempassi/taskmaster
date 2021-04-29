a06c975a16c948a2eea07de23da95462b2a0a99d
firelightflagboy <firelight.flagboy@gmail.com>
2021-04-29 17:52:35 +0200

    add env default value + use static array

--- src/server/default.rs
+++ src/server/default.rs
@@ -6,10 +6,10 @@ pub const UMASK: u16 = 0;
 pub const WORKDIR: &str = ".";
 pub const RELAUNCH_MODE: Relaunch = Relaunch::Never;
 pub const RETRY: u32 = 0;
-pub static EXPECTED_EXIT_CODES: Vec<i32> = Vec::<i32>::new();
+pub const EXPECTED_EXIT_CODES: [i32; 1] = [0];
 pub const SUCCESS_DELAY: u32 = 0;
 pub const STOP_SIGNAL: &str = "QUIT";
 pub const STOP_DELAY: u32 = 2;
 pub const STDOUT: &str = "/dev/null";
 pub const STDERR: &str = "/dev/null";
-// const ENV // TODO
+pub const ENV: [String; 0] = [];
