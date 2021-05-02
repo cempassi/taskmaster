a168e174debbfcc424dd3a0a50c402842822698e
firelightflagboy <firelight.flagboy@gmail.com>
2021-05-02 15:27:13 +0200

    wip when

---
+++ features/steps/when.py
@@ -0,0 +1,36 @@
+from types import SimpleNamespace
+from behave import fixture, when
+from subprocess import Popen, PIPE
+from features.steps.lib.server_proc import ServerProc
+import logging
+
+log = logging.getLogger('when')
+
+TASKMASTER_PATH = 'target/release/taskmaster'
+
+
+@fixture
+def after_feature(ctx):
+    log.warn('calling after feature')
+
+
+def get_args(ctx: SimpleNamespace):
+    l = log.getChild(get_args.__name__)
+    args = ['taskmaster']
+    if ctx.subcommand is not None:
+        args.extend(ctx.subcommand)
+    if ctx.config_file is not None:
+        args.extend(['--config', ctx.config_file])
+    if ctx.verbose_level is not None:
+        args.extend(['--verbose', ctx.verbose_level])
+    return args
+
+
+@when('server is running')
+def run_server(ctx):
+    l = log.getChild(run_server.__name__)
+    ctx.subcommand = ['server']
+    args = get_args(ctx)
+    l.debug(f'args={args}')
+    popen = Popen(args, executable=TASKMASTER_PATH, stdout=PIPE, stderr=PIPE)
+    ctx.server = ServerProc(popen)
