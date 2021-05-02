b0f5f38cad7ed0c46b8b56c395ab4da7c3601cab
firelightflagboy <firelight.flagboy@gmail.com>
2021-05-02 10:17:52 +0200

    add given file

---
+++ features/steps/given.py
@@ -0,0 +1,19 @@
+from behave import given
+import logging
+
+log = logging.getLogger('given')
+
+
+@given("the config file {file} in {lang}")
+def setup_config_file(ctx, file, lang):
+    l = log.getChild(setup_config_file.__name__)
+    l.debug(f'file={file}, lang={lang}')
+    ctx.config_file = file
+    ctx.config_file_type = lang
+
+
+@given("the verbose level as {level}")
+def setup_verbose_level(ctx, level):
+    l = log.getChild(setup_verbose_level.__name__)
+    l.debug(f'level={level}')
+    ctx.verbose_level = level
