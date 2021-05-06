92d291e097e343e40fec777ab83cfa0809b6c11c
firelightflagboy <firelight.flagboy@gmail.com>
2021-05-06 21:27:59 +0200

    create class to manage client

---
+++ features/steps/lib/client_proc.py
@@ -0,0 +1,35 @@
+from typing import List
+from features.steps.lib.taskmaster_utils import TASKMASTER_PATH, get_taskmaster_args
+import logging
+from subprocess import PIPE, Popen
+from types import SimpleNamespace
+
+log = logging.getLogger('client_proc')
+
+
+def get_client_args(config: SimpleNamespace) -> List[str]:
+    l = log.getChild(get_client_args.__name__)
+    args = get_taskmaster_args(config)
+    args.append('client')
+    l.debug(f'args={args}')
+    return args
+
+
+class ClientProc:
+    log = log.getChild(__qualname__)  # type: ignore
+
+    def __init__(self) -> None:
+        cfg = SimpleNamespace()
+        self.args = get_client_args(cfg)
+        self.proc = Popen(self.args, executable=TASKMASTER_PATH,
+                          stdout=PIPE, stderr=PIPE)
+
+    def close(self):
+        self.proc.terminate()
+        try:
+            self.proc.wait(timeout=2)
+        except TimeoutError:
+            self.proc.kill()
+
+    def is_running(self) -> bool:
+        return self.proc.poll() is None
