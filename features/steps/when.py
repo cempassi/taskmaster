1a542cd5869bed0c7f2e7e4133f93fb061473145
firelightflagboy <firelight.flagboy@gmail.com>
2021-05-06 19:27:54 +0200

    rework when server running

--- features/steps/when.py
+++ features/steps/when.py
@@ -6,31 +6,13 @@ import logging
 
 log = logging.getLogger('when')
 
-TASKMASTER_PATH = 'target/release/taskmaster'
-
 
 @fixture
 def after_feature(ctx):
     log.warn('calling after feature')
 
 
-def get_args(ctx: SimpleNamespace):
-    l = log.getChild(get_args.__name__)
-    args = ['taskmaster']
-    if ctx.verbose_level is not None:
-        args.extend(['--verbose', ctx.verbose_level])
-    if ctx.subcommand is not None:
-        args.extend(ctx.subcommand)
-    if ctx.config_file is not None:
-        args.extend(['--config', ctx.config_file])
-    return args
-
-
 @when('server is running')
 def run_server(ctx):
     l = log.getChild(run_server.__name__)
-    ctx.subcommand = ['server']
-    args = get_args(ctx)
-    l.debug(f'args={args}')
-    popen = Popen(args, executable=TASKMASTER_PATH, stdout=2, stderr=2)
-    ctx.server = ServerProc(popen)
+    ctx.server = ServerProc(config=ctx.config_file, verbose=ctx.verbose_level)
