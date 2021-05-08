484b4b70e074f49a9760d317b5c496ed36a11434
firelightflagboy <firelight.flagboy@gmail.com>
2021-05-08 14:55:57 +0200

    skip optional field

--- src/server/reader.rs
+++ src/server/reader.rs
@@ -1,7 +1,7 @@
 use super::watcher::Watcher;
 use super::{default, error, relaunch::Relaunch};
 use libc::{gid_t, mode_t, uid_t};
-use serde::{Deserialize, Serialize};
+use serde::{self, Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::convert::TryFrom;
 use std::fmt;
@@ -12,30 +12,46 @@ pub type ConfigFile = BTreeMap<String, ReadTask>;
 #[derive(Debug, Deserialize, Serialize, Eq, PartialEq, Clone)]
 pub struct ReadTask {
     pub cmd: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub autostart: Option<bool>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub numprocess: Option<u32>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub umask: Option<mode_t>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub workingdir: Option<String>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub stopsignal: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub stopdelay: Option<u32>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub stdout: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub stderr: Option<String>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub retry: Option<u32>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub successdelay: Option<u32>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub exitcodes: Option<Vec<i32>>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub restart: Option<Relaunch>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub env: Option<Vec<String>>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub gid: Option<gid_t>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub uid: Option<uid_t>,
 }
 
