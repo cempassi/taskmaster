2ef48a5c69a6b2eda4474d87b05ca768593d613e
firelightflagboy <firelight.flagboy@gmail.com>
2021-05-09 22:14:00 +0200

    add logging time

--- src/shared/logger.rs
+++ src/shared/logger.rs
@@ -1,7 +1,20 @@
 use log::{Level, Metadata, Record};
+use std::time::Instant;
+
+pub static mut LOGGER: Simple = Simple {
+    level: Level::Debug,
+    now: None,
+};
 
 pub struct Simple {
     pub level: Level,
+    now: Option<Instant>,
+}
+
+impl Simple {
+    pub fn set_instant(&mut self, instant: Instant) {
+        self.now = Some(instant);
+    }
 }
 
 impl log::Log for Simple {
@@ -11,7 +24,12 @@ impl log::Log for Simple {
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            println!("{} - {}", record.level(), record.args())
+            println!(
+                "[{:03}] {:5} - {}",
+                self.now.map_or(0, |time| time.elapsed().as_secs()),
+                record.level(),
+                record.args()
+            )
         }
     }
 
