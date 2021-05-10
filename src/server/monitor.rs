2913f1d9abc3c3c7c03e1596745702fe080f73f8
firelightflagboy <firelight.flagboy@gmail.com>
2021-05-10 21:03:03 +0200

    update state on empty fonction

--- src/server/monitor.rs
+++ src/server/monitor.rs
@@ -28,7 +28,7 @@ impl Monitor {
 
     // Create new Monitoring struct and start the task if required
     pub fn new(task: Task) -> Self {
-        let mon = Monitor::new_only(task);
+        let mut mon = Monitor::new_only(task);
         if mon.task.autostart {
             mon.start();
         }
@@ -39,19 +39,22 @@ impl Monitor {
         self.state = status
     }
 
-    pub fn start(&self) {
+    pub fn start(&mut self) {
         unimplemented!();
+        self.change_state(Status::Active);
     }
 
-    pub fn stop(&self) {
+    pub fn stop(&mut self) {
         unimplemented!();
+        self.change_state(Status::Finished);
     }
 
     pub fn status(&self) -> Status {
         self.state
     }
 
-    pub fn reload(&self, task: Task) {
+    pub fn reload(&mut self, task: Task) {
+        self.change_state(Status::Reloading);
         unimplemented!()
     }
 }
