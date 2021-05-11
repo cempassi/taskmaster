2f874a73931ea2bdeb8de957423439eb829030eb
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2021-05-11 20:38:11 +0200

    use default serde & rework ReadTask (#62)
    * impl Deserialize + Display for signal
    
    * use function instead of static value
    
    * order enum Relaunch
    
    * use serde::default for ReadTask
    
    * remove unwrap_or
    
    * Deserialize Signal using uppercase name
    
    * rework ReadTask using PathBuf

--- src/server/default.rs
+++ src/server/default.rs
@@ -1,16 +1,55 @@
-use super::relaunch::Relaunch;
+use super::{relaunch::Relaunch, signal::Signal};
 use libc::mode_t;
+use std::path::PathBuf;
 
-pub const AUTOSTART: bool = false;
-pub const NUMPROCESS: u32 = 1;
-pub const UMASK: mode_t = 0;
-pub const WORKDIR: &str = ".";
-pub const RELAUNCH_MODE: Relaunch = Relaunch::Never;
-pub const RETRY: u32 = 0;
-pub const EXPECTED_EXIT_CODES: [i32; 1] = [0];
-pub const SUCCESS_DELAY: u32 = 0;
-pub const STOP_SIGNAL: &str = "TERM";
-pub const STOP_DELAY: u32 = 2;
-pub const STDOUT: &str = "/dev/null";
-pub const STDERR: &str = "/dev/null";
-pub const ENV: [String; 0] = [];
+pub fn autostart() -> bool {
+    false
+}
+
+pub fn numprocess() -> u32 {
+    1
+}
+
+pub fn umask() -> mode_t {
+    0
+}
+
+pub fn workdir() -> PathBuf {
+    PathBuf::from(".")
+}
+
+pub fn relaunch_mode() -> Relaunch {
+    Relaunch::Never
+}
+
+pub fn retry() -> u32 {
+    0
+}
+
+pub fn exit_codes() -> Vec<i32> {
+    vec![0]
+}
+
+pub fn success_delay() -> u32 {
+    0
+}
+
+pub fn stop_signal() -> Signal {
+    Signal::Term
+}
+
+pub fn stop_delay() -> u32 {
+    2
+}
+
+pub fn stdout() -> PathBuf {
+    PathBuf::from("/dev/null")
+}
+
+pub fn stderr() -> PathBuf {
+    PathBuf::from("/dev/null")
+}
+
+pub fn env() -> Vec<String> {
+    Vec::new()
+}
