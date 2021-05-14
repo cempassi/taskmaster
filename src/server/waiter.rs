48d47022ec8db5a6e6450d8fd7ff3a76de5820a2
firelightflagboy <firelight.flagboy@gmail.com>
2021-05-14 16:09:55 +0200

    wip wait part of waiter

--- src/server/waiter.rs
+++ src/server/waiter.rs
@@ -1,7 +1,12 @@
 use super::message::Inter;
+use nix::{
+    sys::wait::{waitpid, WaitStatus},
+    unistd::Pid,
+};
 use std::{
     sync::{
         atomic::{AtomicUsize, Ordering},
+        mpsc::Sender,
         Arc,
     },
     thread::{self, JoinHandle},
@@ -20,10 +25,30 @@ impl Waiter {
         }
     }
 
-    pub fn wait_children(&mut self) {
+    pub fn wait_children(&mut self, sender: Sender<Inter>) {
         self.counter.fetch_add(1, Ordering::SeqCst);
         if self.thread.is_none() {
-            self.thread = Some(thread::spawn(|| {}));
+            let counter = self.counter.clone();
+            self.thread = Some(thread::spawn(move || {
+                while counter.load(Ordering::SeqCst) > 0 {
+                    match waitpid(Pid::from_raw(-1), None) {
+                        Ok(status) => match status {
+                            WaitStatus::Exited(pid, _) | WaitStatus::Signaled(pid, _, _) => {
+                                log::debug!("a process has exited {:?}", status);
+                                sender.send(Inter::ChildrenExited(pid, status)).unwrap()
+                            }
+                            _ => {
+                                log::warn!("exit status {:?} not handled", status);
+                            }
+                        },
+                        Err(error) => {
+                            log::error!("error while using waitpid: {}", error);
+                            panic!("error while waiting for subproccess");
+                        }
+                    }
+                }
+                log::debug!("wait counter at zero, finished waiting for subprocess");
+            }));
         } else {
             log::debug!("waiting thread already running");
         }
