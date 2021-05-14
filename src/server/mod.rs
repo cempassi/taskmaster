f163ecd6fdd57b6df19bf527f37d5c5f5128ebc6
firelightflagboy <firelight.flagboy@gmail.com>
2021-05-14 11:15:18 +0200

    use server as general state

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -15,33 +15,58 @@ mod state;
 mod task;
 mod watcher;
 
-use self::watcher::Watcher;
-use self::{listener::Listener, state::State};
+use self::{
+    communication::Communication, listener::Listener, message::Inter, state::State,
+    watcher::Watcher,
+};
+
+struct Server {
+    state: State,
+    watcher: Watcher,
+    msg_listener: Listener,
+}
 
 pub fn start(config: &str) -> Result<(), error::Taskmaster> {
-    let (sender, receiver) = channel();
+    let (sender, receiver) = channel::<Inter>();
     let mut state = State::new();
     let mut watcher = Watcher::try_from(config).unwrap();
     let mut listener = Listener::new();
 
     listener.run(sender.clone());
     watcher.run(sender.clone());
+
+    let mut server = Server {
+        state,
+        watcher,
+        msg_listener: listener,
+    };
+
     signal::handle_signals(sender)?;
     loop {
-        if let Ok(com) = receiver.recv() {
-            log::info!("received message: {:?}", com.message);
-            match com.message {
-                Message::Reload => state.reload(&watcher),
-                Message::Start(task) if com.channel.is_some() => {
-                    state.start(&task);
-                }
-                Message::Start(task) => state.start(&task),
-                Message::Stop(task) => state.stop(&task),
-                Message::List => state.list(&com.channel.unwrap()),
-                Message::Status(taskname) => state.status(&taskname, &com.channel.unwrap()),
-                Message::Quit => break,
-            };
+        if let Ok(message) = receiver.recv() {
+            log::info!("received internal message: {:?}", message);
+            match message {
+                Inter::ChildrenExited(pid, status) => unimplemented!(),
+                Inter::ChildrenToWait => unimplemented!(),
+                Inter::FromClient(com) => server.handle_client_message(&mut state, com),
+            }
         };
     }
     Ok(())
+}
+
+impl Server {
+    fn handle_client_message(&mut self, com: Communication) {
+        match com.message {
+            Message::Reload => self.state.reload(&self.watcher),
+            Message::Start(taskname) if com.channel.is_some() => {
+                self.state.start(&taskname);
+            }
+            Message::Start(taskname) => self.state.start(&taskname),
+            Message::Stop(taskname) => self.state.stop(&taskname),
+            Message::List => self.state.list(&com.channel.unwrap()),
+            Message::Status(taskname) => self.state.status(&taskname, &com.channel.unwrap()),
+            Message::Quit => break,
+        };
+    }
 }
