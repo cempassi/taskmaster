d3944b554de9dc37a60b5cb11871c2dbbe30be02
firelight.flagboy <firelight.flagboy@gmail.com>
2021-06-07 17:57:01 +0200

    add script to install taskmaster

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 INSTALL_DIR=/opt/taskmaster
 
 ~/.cargo/bin/cargo install --path . --root $INSTALL_DIR
-cp -vR configs $INSTALL_DIR/configs
 
+install -v -d $INSTALL_DIR/configs
+install -v configs/* $INSTALL_DIR/configs
 install -v taskmaster.service /usr/lib/systemd/system/taskmaster.service
