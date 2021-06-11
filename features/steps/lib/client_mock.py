f3c05342d092823082f7e913c141d229e9456e40
firelight.flagboy <firelight.flagboy@gmail.com>
2021-06-11 16:46:15 +0200

    client only try to connect when sending cmd

--- features/steps/lib/client_mock.py
+++ features/steps/lib/client_mock.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 from enum import Enum
+from socket import socket
 from features.steps.lib.taskmaster_utils import connect_to_socket, scan_tasks
 from json import dumps
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, TextIO, Union
 
 
 log = logging.getLogger('client_mock')
@@ -27,8 +28,7 @@ class ClientMock:
     log = log.getChild(__qualname__)  # type: ignore
 
     def __init__(self) -> None:
-        self.sock = connect_to_socket()
-        self.sock_file = self.sock.makefile()
+        pass
 
     @staticmethod
     def build_command(name: str, extra: Dict[str, Any] = {}) -> Dict[str, Any]:
@@ -36,21 +36,33 @@ class ClientMock:
         ClientMock.log.debug(f'cmd={cmd}')
         return cmd
 
-    def send_command(self, command: str):
+    def send_command(self, command: str) -> Union[socket, TextIO]:
+        sock = connect_to_socket()
+        sockfile = sock.makefile()
+
         self.log.debug(f'sending command: {command}')
-        self.sock.send(command.encode())
+        sock.send(command.encode())
+        return (sock, sockfile)
 
-    def read_data(self, max_size: int) -> bytes:
-        return self.sock.recv(max_size)
+    def read_data(self, sock: socket, max_size: int) -> bytes:
+        return sock.recv(max_size)
 
-    def readlines(self, hint: int = -1) -> list[str]:
-        return self.sock_file.readlines(hint)
+    def readlines(self, sock_file: TextIO, hint: int = -1) -> list[str]:
+        return sock_file.readlines(hint)
 
-    def readline(self, size: int) -> str:
-        return self.read_data(size).decode()
+    def readline(self, sock: socket, size: int) -> str:
+        return self.read_data(sock, size).decode()
 
     def send_list(self) -> Dict[str, object]:
         """send list command to server"""
-        command = dumps(ClientMock.build_command(ClientCommand.LIST))
+        raw_command = ClientMock.build_command(ClientCommand.LIST)
+        command = dumps(raw_command)
+        sock, _ = self.send_command(command)
+        return scan_tasks(self.readline(sock, 4096))
+
+    def send_start(self, taskname: str):
+        """send start command to server"""
+        raw_command = ClientMock.build_command(
+            ClientCommand.START, {'id': taskname})
+        command = dumps(raw_command)
         self.send_command(command)
-        return scan_tasks(self.readline(4096))
