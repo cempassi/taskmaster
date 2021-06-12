5007327aae9ed3f44b855d90dd0fbe9c7115840f
firelight.flagboy <firelight.flagboy@gmail.com>
2021-06-12 10:15:31 +0200

    add namespace that use lock

--- features/steps/lib/utils.py
+++ features/steps/lib/utils.py
@@ -1,6 +1,19 @@
+from threading import Lock
 from typing import Any, Union
 
 
 class Namespace(dict):
     def __getattr__(self, attr) -> Union[Any, None]:
         return self.get(attr)
+
+
+class NamespaceLock(Namespace):
+    def __init__(self, lock: Lock, *args, **kwargs):
+        self.lock = lock
+        super().__init__(*args, **kwargs)
+
+    def __getattr__(self, attr) -> Union[Any, None]:
+        self.lock.acquire()
+        data = super().__getattr__(attr)
+        self.lock.release()
+        return data
