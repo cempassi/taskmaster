e85c2f77b58c8a3384b7df06eccb95d1be8f03a3
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2021-06-21 09:46:02 +0200

    Refactor/signal message (#116)
    * add sigstr
    
    * refactor signal handler

--- src/server/signal.rs
+++ src/server/signal.rs
@@ -1,5 +1,5 @@
 use signal_hook::{
-    consts::{SIGHUP, SIGINT, SIGTERM, SIGQUIT},
+    consts::{SIGHUP, SIGINT, SIGQUIT, SIGTERM},
     iterator::Signals,
 };
 use std::sync::mpsc::Sender;
@@ -7,6 +7,15 @@ use std::sync::mpsc::Sender;
 use super::inter::Inter;
 use crate::error;
 
+fn sigstr(signum: i32) -> &'static str {
+    match signum {
+        SIGINT => "SIGINT",
+        SIGTERM => "SIGTERM",
+        SIGHUP => "SIGHUP",
+        SIGQUIT => "SIGQUIT",
+        _ => "unknown",
+    }
+}
 pub fn handle_signals(sender: Sender<Inter>) -> Result<(), error::Taskmaster> {
     let mut watching_signals = match Signals::new(&[SIGHUP, SIGINT, SIGTERM, SIGQUIT]) {
         Ok(c) => c,
@@ -19,16 +28,8 @@ pub fn handle_signals(sender: Sender<Inter>) -> Result<(), error::Taskmaster> {
                     log::debug!("received SIGHUP, send Reload message");
                     sender.send(Inter::Reload).unwrap()
                 }
-                SIGINT => {
-                    log::debug!("received SIGINT, sending Quit message");
-                    sender.send(Inter::Quit).unwrap()
-                }
-                SIGTERM => {
-                    log::debug!("received SIGTERM, sending Quit message");
-                    sender.send(Inter::Quit).unwrap()
-                }
-                SIGQUIT => {
-                    log::debug!("received SIGQUIT, sending Quit message");
+                SIGINT | SIGTERM | SIGQUIT => {
+                    log::debug!("received {}, sending Quit message", sigstr(sig));
                     sender.send(Inter::Quit).unwrap()
                 }
                 _ => {
@@ -39,4 +40,19 @@ pub fn handle_signals(sender: Sender<Inter>) -> Result<(), error::Taskmaster> {
         }
     });
     Ok(())
+}
+
+#[cfg(test)]
+mod test_signal {
+    use super::sigstr;
+    use signal_hook::consts::{SIGCONT, SIGHUP, SIGINT, SIGQUIT, SIGTERM};
+
+    #[test]
+    fn test_sigstr() {
+        assert_eq!(sigstr(SIGTERM), "SIGTERM");
+        assert_eq!(sigstr(SIGQUIT), "SIGQUIT");
+        assert_eq!(sigstr(SIGINT), "SIGINT");
+        assert_eq!(sigstr(SIGHUP), "SIGHUP");
+        assert_eq!(sigstr(SIGCONT), "unknown");
+    }
 }
